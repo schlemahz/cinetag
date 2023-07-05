f19cacad3a4725e6ff5856948cd7854f6557848f
schlemahz <schlemahz@gmail.com>
2023-07-05 14:59:31 -0300

    Ultima versao
    Ultima versao

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
+import styles from './Cabecalho.module.css';
+import CabecalhoLink from '../CabecalhoLink';
+
+export default function Cabecalho() {
+    return (
+        <header className={styles.cabecalho}>
+            <Link to="./">
+                <img src={logo} alt="Logo do cinetag"></img>
+            </Link>
+            <nav>
+                <CabecalhoLink url="./">
+                    Home
+                </CabecalhoLink>
+                <CabecalhoLink url="./favoritos">
+                    Favoritos
+                </CabecalhoLink>
+            </nav>
+
+        </header>
+    )
+}
